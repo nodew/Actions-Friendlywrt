c88fa632554561484fed0922160a1128c04edbff
Qiao Wang <wangqiao11@hotmail.com>
2022-01-06 23:29:59 +0800

    Update workflow

--- patches.sh
+++ patches.sh
@@ -1,2 +1,2 @@
-sed -i '/^tools-y :=.*/a tools-y += ucl upx' tools/Makefile
-sed -i '/^# builddir dependencies/a $(curdir)/upx/compile := $(curdir)/ucl/compile' tools/Makefile
+sed -i '/^tools-y :=.*/a tools-y += ucl upx' friendlywrt/tools/Makefile
+sed -i '/^# builddir dependencies/a $(curdir)/upx/compile := $(curdir)/ucl/compile' friendlywrt/tools/Makefile
